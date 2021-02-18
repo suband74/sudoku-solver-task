67b4e230a1cc9c4e44d48affba46bc68795b701f
suband74 <66212912+suband74@users.noreply.github.com>
2021-02-18 22:39:21 +0700

    Sudoku with cli 120221 (#9)
    Introduce CLI

---
+++ solve_sudoku.py
@@ -0,0 +1,59 @@
+import json
+from collections import Counter
+from typing import List
+
+import click
+
+from decision_sudoku import solve
+
+
+def search_same_numbers(s):
+    for i in s:
+        x = Counter(i)
+        for key in x:
+            if key != 0 and x[key] != 1:
+                raise ValueError('The same numbers in the string (column,block). Unsolvable sudoku')
+
+
+def validate_sudoku(s: List[List[int]]):
+    """
+    проверка входящего судоку на валидность
+    :param s: List[List[int]]
+    :return:
+    """
+    for u in s:
+        for r in u:
+            if not isinstance(r, int) or 9 < r or r < 0:
+                raise TypeError('sudoku can only enter numbers from 0 to 9 inclusive')
+
+    search_same_numbers(s)
+
+    ss = list(zip(*s))
+    search_same_numbers(ss)
+
+    d = [[], [], [], [], [], [], [], [], []]
+    n = 0
+    for m in range(0, 7, 3):
+        for k in range(0, 7, 3):
+            for i in range(k, k + 3):
+                for j in range(m, m + 3):
+                    d[n].append(s[i][j])
+            n += 1
+    search_same_numbers(d)
+
+
+@click.command()
+@click.argument('file_start_sudoku', type=click.Path(exists=True))
+@click.argument('file_result_sudoku', type=click.Path(exists=False))
+def main(file_start_sudoku, file_result_sudoku):
+    with open(file_start_sudoku) as f:
+        data = json.load(f)
+        sudoku = data['table']
+        validate_sudoku(sudoku)
+        solve(sudoku)
+        with open(file_result_sudoku, 'w') as file:
+            json.dump(data, file)
+
+
+if __name__ == '__main__':
+    main()
--- test_decision_sudoku.py
+++ test_decision_sudoku.py
@@ -1,4 +1,107 @@
 from decision_sudoku import _box_slice, solve, _find_unassigned
+from solve_sudoku import validate_sudoku
+import pytest
+
+
+def test_validate_sudoku_str():
+    """
+    Проверка входящего судоку на одинаковые цифры в строке
+    :return: True or False
+    """
+    s = [[1, 2, 3, 4, 4, 6, 7, 8, 9],
+         [4, 0, 6, 7, 8, 9, 1, 2, 3],
+         [7, 8, 9, 1, 2, 3, 4, 0, 6],
+         [2, 3, 4, 0, 6, 7, 8, 9, 1],
+         [0, 6, 7, 8, 9, 1, 2, 3, 4],
+         [8, 9, 1, 2, 3, 4, 0, 6, 7],
+         [3, 4, 0, 6, 7, 8, 9, 1, 2],
+         [6, 7, 8, 9, 1, 2, 3, 4, 0],
+         [9, 0, 2, 3, 4, 0, 6, 7, 8]]
+    with pytest.raises(ValueError) as exc:
+        validate_sudoku(s)
+        assert 'The same numbers in the string (column,block). Unsolvable sudoku' == str(exc.value)
+
+
+def test_validate_sudoku_column():
+    """
+        Проверка входящего судоку на одинаковые цифры в столбце
+        :return: True or False
+        """
+    s = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [4, 0, 6, 7, 8, 9, 1, 2, 3],
+         [7, 8, 9, 1, 2, 3, 4, 0, 6],
+         [2, 3, 4, 0, 6, 7, 8, 9, 1],
+         [0, 6, 7, 8, 9, 1, 2, 3, 4],
+         [2, 9, 1, 0, 3, 4, 0, 6, 7],
+         [3, 4, 0, 6, 7, 8, 9, 1, 2],
+         [6, 7, 8, 9, 1, 2, 3, 4, 0],
+         [9, 0, 2, 3, 4, 0, 6, 7, 8]]
+    with pytest.raises(ValueError) as exc:
+        validate_sudoku(s)
+        assert 'The same numbers in the string (column,block). Unsolvable sudoku' == str(exc.value)
+
+
+def test_validate_sudoku_block():
+    """
+        Проверка входящего судоку на одинаковые цифры в блоке 3*3
+        :return: True or False
+        """
+    s = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    with pytest.raises(ValueError) as exc:
+        validate_sudoku(s)
+        assert 'The same numbers in the string (column,block). Unsolvable sudoku' == str(exc.value)
+
+
+def test_validate_sudoku_type():
+    """
+        Проверка входящего судоку на тип данных(допускаются только целые цифры)
+        :return: True or False
+        """
+    s = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 'm', 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    with pytest.raises(TypeError) as exc:
+        validate_sudoku(s)
+        assert 'sudoku can only enter numbers from 0 to 9 inclusive' == str(exc.value)
+
+
+def test_validate_sudoku_num():
+    """
+            Проверка входящего судоку на интервал входящих цифр(допускаются только цифры от 0 до 9)
+            :return: True or False
+            """
+    s = [
+        [1, -1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    with pytest.raises(TypeError) as exc:
+        validate_sudoku(s)
+        assert 'sudoku can only enter numbers from 0 to 9 inclusive' == str(exc.value)
 
 
 def test__box_slice():
@@ -112,32 +215,3 @@ def test_solved_sudoku():
         [4, 1, 3, 5, 9, 6, 2, 7, 8],
         [5, 9, 7, 8, 2, 1, 3, 4, 6]
     ]
-
-
-def test_completed_random_number():
-    """
-    Работа функции с заполненным случайными числами судоку
-    :return: Булево
-    """
-    s = [
-        [1, 3, 6, 6, 7, 5, 8, 3, 4],
-        [3, 5, 8, 7, 1, 2, 6, 9, 7],
-        [9, 7, 4, 4, 8, 3, 1, 2, 5],
-        [2, 3, 5, 9, 3, 8, 7, 6, 9],
-        [6, 1, 9, 0, 5, 9, 4, 1, 3],
-        [7, 2, 1, 3, 6, 9, 5, 8, 2],
-        [8, 6, 2, 5, 3, 4, 9, 5, 1],
-        [4, 1, 3, 5, 9, 6, 7, 7, 8],
-        [5, 9, 7, 9, 2, 1, 5, 4, 6]
-    ]
-    solve(s)
-    assert s == [[1, 3, 6, 6, 7, 5, 8, 3, 4],
-                 [3, 5, 8, 7, 1, 2, 6, 9, 7],
-                 [9, 7, 4, 4, 8, 3, 1, 2, 5],
-                 [2, 3, 5, 9, 3, 8, 7, 6, 9],
-                 [6, 1, 9, 2, 5, 9, 4, 1, 3],
-                 [7, 2, 1, 3, 6, 9, 5, 8, 2],
-                 [8, 6, 2, 5, 3, 4, 9, 5, 1],
-                 [4, 1, 3, 5, 9, 6, 7, 7, 8],
-                 [5, 9, 7, 9, 2, 1, 5, 4, 6]
-                 ]
